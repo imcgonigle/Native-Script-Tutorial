a9b9c9ecff0a7f9c998df80b8289f008301af790
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-11 22:54:56 -0600

    add submit-button class to sign in button

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from "@angular/core";
         autocorrect="false" autocapitalizationType="none"></TextField>
       <TextField hint="Password" secure="true"></TextField>
 
-      <Button text="Sign in"></Button>
+      <Button text="Sign in" class="submit-button"></Button>
       <Button text="Sign up for Groceries"></Button>
     </StackLayout>
   `,
