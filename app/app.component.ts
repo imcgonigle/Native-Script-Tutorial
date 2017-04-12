f29af54b64e18be731e4edd5389353bf31f4f4d1
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-11 22:52:28 -0600

    add style urls

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,6 +11,7 @@ import { Component } from "@angular/core";
       <Button text="Sign in"></Button>
       <Button text="Sign up for Groceries"></Button>
     </StackLayout>
-  `
+  `,
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 export class AppComponent {}
