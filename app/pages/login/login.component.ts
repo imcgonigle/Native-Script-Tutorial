60aa7ec694cab9a72dcfa0c0296ea817c58884d9
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-13 14:53:44 -0600

    update login to use user.service login

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 import { User } from "../../shared/user/user";
 import { UserService } from "../../shared/user/user.service";
@@ -14,7 +15,7 @@ export class LoginComponent {
   user: User;
   isLoggingIn = true;
 
-  constructor(private userService: UserService) {
+  constructor(private router: Router, private userService: UserService) {
     this.user = new User();
     this.user.email = 'ian@emailplace.com';
     this.user.password = 'password';
@@ -29,19 +30,23 @@ export class LoginComponent {
   }
 
   login() {
-    // TODO: Define
+    this.userService.login(this.user)
+      .subscribe(
+        () => this.router.navigate(["/list"]),
+        (error) => alert("Unfortunately we could not find your account.")
+      );
   }
 
   signUp() {
-  this.userService.register(this.user)
-    .subscribe(
-      () => {
-        alert("Your account was successfully created.");
-        this.toggleDisplay();
-      },
-      () => alert("Unfortunately we were unable to create your account.")
-    );
-}
+    this.userService.register(this.user)
+      .subscribe(
+        () => {
+          alert("Your account was successfully created.");
+          this.toggleDisplay();
+        },
+        () => alert("Unfortunately we were unable to create your account.")
+      );
+  }
   
   toggleDisplay() {
     this.isLoggingIn = !this.isLoggingIn;
