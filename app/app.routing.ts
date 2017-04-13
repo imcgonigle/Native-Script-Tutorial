9be513590be567d36e89a366f4356a6cc4d446b1
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-13 14:38:33 -0600

    remove spacing

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -5,7 +5,6 @@ import { ListComponent } from "./pages/list/list.component";
 export const routes = [
   { path: "", component: LoginComponent },
   { path: "list", component: ListComponent }
-
 ];
 
 export const navigatableComponents = [
