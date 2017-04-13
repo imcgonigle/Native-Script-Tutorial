3eb04217b1307ba0e246609959813ea2fdbed55e
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-13 14:20:14 -0600

    Create list component

--- app/pages/list/list.component.js
+++ app/pages/list/list.component.js
@@ -1,1 +1,16 @@
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibGlzdC5jb21wb25lbnQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJsaXN0LmNvbXBvbmVudC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlc0NvbnRlbnQiOlsiIl19
+"use strict";
+var core_1 = require("@angular/core");
+var ListComponent = (function () {
+    function ListComponent() {
+    }
+    return ListComponent;
+}());
+ListComponent = __decorate([
+    core_1.Component({
+        selector: "list",
+        templateUrl: "./list.html",
+        styleUrls: ["./list-common.css", "./list.component.ts"]
+    })
+], ListComponent);
+exports.ListComponent = ListComponent;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibGlzdC5jb21wb25lbnQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJsaXN0LmNvbXBvbmVudC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsc0NBQTBDO0FBUTFDLElBQWEsYUFBYTtJQUExQjtJQUE0QixDQUFDO0lBQUQsb0JBQUM7QUFBRCxDQUFDLEFBQTdCLElBQTZCO0FBQWhCLGFBQWE7SUFOekIsZ0JBQVMsQ0FBQztRQUNULFFBQVEsRUFBRSxNQUFNO1FBQ2hCLFdBQVcsRUFBRSxhQUFhO1FBQzFCLFNBQVMsRUFBRSxDQUFDLG1CQUFtQixFQUFFLHFCQUFxQixDQUFDO0tBQ3hELENBQUM7R0FFVyxhQUFhLENBQUc7QUFBaEIsc0NBQWEiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBDb21wb25lbnQgfSBmcm9tICdAYW5ndWxhci9jb3JlJztcblxuQENvbXBvbmVudCh7XG4gIHNlbGVjdG9yOiBcImxpc3RcIixcbiAgdGVtcGxhdGVVcmw6IFwiLi9saXN0Lmh0bWxcIixcbiAgc3R5bGVVcmxzOiBbXCIuL2xpc3QtY29tbW9uLmNzc1wiLCBcIi4vbGlzdC5jb21wb25lbnQudHNcIl1cbn0pXG5cbmV4cG9ydCBjbGFzcyBMaXN0Q29tcG9uZW50IHt9Il19
---
+++ app/pages/list/list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: "list",
+  templateUrl: "./list.html",
+  styleUrls: ["./list-common.css", "./list.component.ts"]
+})
+
+export class ListComponent {}
