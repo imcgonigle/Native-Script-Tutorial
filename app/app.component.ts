adfeedf14f89e85d407cb5a4f0ad4d995a91d6c2
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-12 18:55:22 -0600

    Point templateUrl to the login template

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,17 +3,7 @@ import { User } from "./shared/user/user";
 
 @Component({
   selector: "my-app",
-  template: `
-    <StackLayout>
-      <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
-      <TextField hint="Email Address" keyboardType="email" [(ngModel)]="user.email"
-        autocorrect="false" autocapitalizationType="none"></TextField>
-      <TextField hint="Password" secure="true" [(ngModel)]="user.password"></TextField>
-
-      <Button [text]="isLoggedIn ? 'Sign in' : 'Sign up'" class="submit-button" (tap)="submit()"></Button>
-      <Button [text]="isLoggedIn ? 'Sign up' : 'Back to login'" (tap)="toggleDisplay()"></Button>
-    </StackLayout>
-  `,
+  templateUrl: "pages/login/login.html",
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 export class AppComponent {
