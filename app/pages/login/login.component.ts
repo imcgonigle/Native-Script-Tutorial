9deb41058639c9c7ecdd80c77457f7fb22147b4a
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-14 16:40:13 -0600

    import setHintColor and TextField

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -8,6 +8,9 @@ import { View } from "ui/core/view";
 import { User } from "../../shared/user/user";
 import { UserService } from "../../shared/user/user.service";
 
+import { setHintColor } from "../../utils/hint-util";
+import { TextField } from "ui/text-field";
+
 @Component({
   selector: "my-app",
   providers: [UserService],
