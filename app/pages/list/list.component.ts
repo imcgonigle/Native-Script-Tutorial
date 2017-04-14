9281edc5cfcd85d9fb7f72be93898d72096ad201
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-14 16:31:10 -0600

    add share function

--- app/pages/list/list.component.ts
+++ app/pages/list/list.component.ts
@@ -57,4 +57,12 @@ export class ListComponent implements OnInit {
         }
       )
   }
+  share() {
+    let list = [];
+    for (let i = 0, size = this.groceryList.length; i < size ; i++) {
+      list.push(this.groceryList[i].name);
+    }
+    let listString = list.join(", ").trim();
+    SocialShare.shareText(listString);
+  }
 }
